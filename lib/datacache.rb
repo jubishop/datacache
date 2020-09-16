b3bafccd371abb355d4ca086d65aa0bd44324f2f
Justin Bishop <jubishop@gmail.com>
2020-09-15 20:04:36 -0700

    fancier datacache

--- lib/datacache.rb
+++ lib/datacache.rb
@@ -11,19 +11,26 @@ class DataCache
   end
 
   def fetch(key, &block)
-    raise ArgumentError, 'Block must be given to DataCache.fetch' unless block
+    return @cache.fetch(key) if fresh?(key)
 
-    if @cache.key?(key) && @store_times.fetch(key) + @timeout > Time.now
-      return @cache.fetch(key)
-    end
+    return set(key, &block)
+  end
+
+  def set(key, &block)
+    raise ArgumentError, 'Block must be given to DataCache.fetch' unless block
 
     @cache[key] = block.run
     @store_times[key] = Time.now
     return @cache.fetch(key)
   end
 
+  def fresh?(key)
+    return @cache.key?(key) && @store_times.fetch(key) + @timeout > Time.now
+  end
+
   def invalidate(key)
     @cache.delete(key)
     @store_times.delete(key)
+    return true
   end
 end
